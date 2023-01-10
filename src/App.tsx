ab02695b57c9cd84636317705b8cdcaa5160c4ea
GarethBeer <gareth.beer1989@outlook.com>
2023-01-10 16:51:26 +0000

    [update](app component) adding basic azure call

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
 import { useEffect, useState } from 'react';
+import Home from './components/Home/Home';
 import LaunchScreen from './components/LaunchScreen/LaunchScreen';
 import LoginScreen from './components/LogInScreen/LoginScreen';
 import './styles/App.scss';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState<any>(null)
+  const [showLaunchScreen, setShowLaunchScreen] = useState(true)
 
   useEffect(() => {
+    getUserInfo().then((res) => {
+      const fakeUser = {
+      name: 'Gareth Beer'
+    }
+      setLoggedIn(res ? true : true);
+      setLoggedInUser(fakeUser)
+  })
     setTimeout(() => {
-      setLoggedIn(true)
+      setShowLaunchScreen(false)
     }, 5000)
   }, [])
 
+
+const getUserInfo = async () => {
+    try {
+        const response = await fetch('/.auth/me');
+        const payload = await response.json();
+        const { clientPrincipal } = payload;
+
+        if (!clientPrincipal) {
+        
+            return null;
+        } else {
+          return clientPrincipal
+        }
+        
+    } catch (error) {
+    
+        return undefined;
+    }
+}
   return (
     <div className="App">
-    {!loggedIn ? <LaunchScreen /> :
-      <LoginScreen />}
+      {showLaunchScreen ? <LaunchScreen /> :
+        <>
+          {loggedIn &&  <Home user={loggedInUser} />}
+          {!loggedIn && <LoginScreen /> }  
+        </>
+    }
       
     </div>
   );
